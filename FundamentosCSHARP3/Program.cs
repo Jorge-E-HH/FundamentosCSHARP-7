8e00393b652a5b1f8a5ff9f5bab8c5266f08a78d
ThethingOne <Jaydersmk@gmail.com>
2022-12-01 14:46:39 -0600

    leccion 6

---
+++ FundamentosCSHARP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using FundamentosCSHARP.Models;
+using FundamentosCSHARP3.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace FundamentosCSHARP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cerveza cerveza = new Cerveza(10, "Cerveza");
+            //string miJson = JsonSerializer.Serialize(cerveza);
+            //File.WriteAllText("objeto.txt", miJson);
+
+            string miJson = File.ReadAllText("objeto.txt");
+            Cerveza cerveza = JsonSerializer.Deserialize<Cerveza>(miJson);
+
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso c# nobb/Master. 
+# leccion 7. 
+#Objeto, persona/herramientas,bla, bla.
+#Interfaces.
+#Json/object txt/bin/crear txt, desde c#/cargar desde txt.
+
+# Desarrollado en C#
+
+ # Créditos
+  [J. Hernandez](https://github.com/Jorge-E-HH) autor original del contenido.
